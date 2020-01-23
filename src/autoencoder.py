6a185bfa626a03857d4d24be8c901be6ad0ead5a
Sifan Jiang <jasper.sifan@gmail.com>
2020-01-23 16:05:40 +0100

    some psudo

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -1,14 +1,23 @@
 from keras.layers import Input, Dense
 from keras.models import Model
 
+import math
+import numpy as np
+
+
 class Autoencoder:
 
-    def __init__(self, input, num_node, rate=1e-4, decay=1e-6, batch=128):
-        self.num_layer = len(num_node)
-        self.num_node = num_node
-        self.input_layer = Input()
-        self.encoder_layers = []
-        self.decoder_layers = []
+    def __init__(self, input, num_node_list, rate=1e-4, decay=1e-6, batch=128):
+        size_input = np.size(input)
+        self.input = np.reshape(input.copy(), (size_input, 1))
+
+        num_node_list_reverse = num_node_list.reverse()
+        self.num_node_list = [size_input] + num_node_list + num_node_list_reverse[1:] + [size_input]
+        self.autoencoder_layer_list = []
+
+        self.rate = rate
+        self.decay = decay
+        self.batch = batch
 
     def build(self):
         pass
@@ -19,10 +28,17 @@ class Autoencoder:
     def get_latent(self):
         pass
 
-    def set_latent(self):
+    def set_latent(self, latent=None):
+        """Set the latent variables of the autoencoder.
+        
+        :param latent: [description], defaults to None
+        :type latent: [type], optional
+        """
+
         pass
 
 
 if __name__ == "__main__":
+    skeleton_input = np.random.normal(size=(5, 25))
     pass
     # skeleton_autoencoder = Autoencoder(input, [512, 256, 128, 64, 7])
---
+++ src/motiTrajSync.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+class MotionTrajectorySynchronization:
+
+    def __init__(self, nopt_seq):
+        self.nopt_seq = nopt_seq.copy()
+        self.opt_seq = np.zeros_like(self.nopt_seq)
+        
+        self.P = np.size(self.nopt_seq, axis=1)
+        
+        self.Q1 = 5 * np.eye(self.P)
+        self.Q2 = 0.1 * np.eye(self.P)
+        self.Q3 = 10 * np.eye(self.P)
+
+    def getRobotConstraints(self):
+        pass
+        
+    def getRobotDynamics(self):
+        pass
+
+    def optimizeSequence(self):
+        pass
+
+    def constrainedOptimization(self):
+        pass
+
+    def piecewiseOptimization(self):
+        pass
+
+    def jointConstraintViolated(self) -> bool:
+        return True
+
+    def notLargeAmplitudeMotion(self) -> bool:
+        return True
