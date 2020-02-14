efa61dbe060e7ae1ad3afd5ffa2928b51636a47d
JasperRice <jasper.sifan@gmail.com>
2020-02-14 10:33:34 +0100

    Include FPS

--- src/Interface.py
+++ src/Interface.py
@@ -7,7 +7,7 @@ import time
 start = time.time()
 
 
-def main(IP, PORT):
+def main(IP, PORT, FPS):
     motion  = ALProxy("ALMotion", IP, PORT)
     motion.post.setAngles(['LShoulderRoll', 'LShoulderPitch', 'LElbowRoll', 'LElbowYaw'],
                           [0.0, 0.0, 0.0, 0.0],
@@ -23,5 +23,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", type=str, default="127.0.0.1", help="Address IP of the NAO robot.")
     parser.add_argument("--port", type=int, default=9559, help="Port number of the NAO robot.")
+    parser.add_argument("--fps", type=int, default=30, help="FPS of the gesture.")
     args = parser.parse_args()
-    main(args.ip, args.port)
+    main(args.ip, args.port, args.fps)
