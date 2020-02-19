1e690124d3b0bcf16e06e82be5fa715ebf7b86da
JasperRice <jasper.sifan@gmail.com>
2020-02-19 15:16:41 +0100

    Try angleInterpolation

--- src/Interface.py
+++ src/Interface.py
@@ -8,21 +8,54 @@ start = time.time()
 
 
 def main(IP, PORT, FPS):
+    """Interface of the motion retrageting
+    
+    :param IP: Adress IP of the NAO robot
+    :type IP: str
+    :param PORT: Port number of the NAO robot
+    :type PORT: int
+    """
     motion  = ALProxy("ALMotion", IP, PORT)
-    motion.post.setAngles(['LShoulderRoll', 'LShoulderPitch', 'LElbowRoll', 'LElbowYaw'],
-                          [0.0, 0.0, 0.0, 0.0],
-                          1.0)
-    time.sleep(1.0 / FPS)
+    # memory  = ALProxy("ALMemory", IP, PORT)
 
-    motion.post.setAngles(['LShoulderRoll', 'LShoulderPitch', 'LElbowRoll', 'LElbowYaw'],
-                          [0.0, 0.5, 0.0, 0.0],
-                          1.0)
+    # for _ in range(50):
+        # motion.post.setAngles(['LShoulderRoll', 'LShoulderPitch', 'LElbowRoll', 'LElbowYaw'],
+        #                     [0.0, 0.0, 0.0, 0.5],
+        #                     1.0)
+        # time.sleep(1.0/5.0)
+
+        # motion.post.setAngles(['LShoulderRoll', 'LShoulderPitch', 'LElbowRoll', 'LElbowYaw'],
+        #                     [0.0, 0.5, 0.5, -0.5],
+        #                     1.0)
+        # time.sleep(1.0/5.0)
+
+    motion.angleInterpolation('LShoulderPitch', 0.0, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.1, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.2, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.3, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.4, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.5, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.6, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.7, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.8, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.9, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 1.0, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.9, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.8, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.7, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.6, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.5, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.4, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.3, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.2, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.1, 1.0/20, True)
+    motion.angleInterpolation('LShoulderPitch', 0.0, 1.0/20, True)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", type=str, default="127.0.0.1", help="Address IP of the NAO robot.")
     parser.add_argument("--port", type=int, default=9559, help="Port number of the NAO robot.")
-    parser.add_argument("--fps", type=int, default=30, help="FPS of the gesture.")
+    parser.add_argument("--fps", type=int, default=30, help="FPS of motion of the NAO robot.")
     args = parser.parse_args()
     main(args.ip, args.port, args.fps)
