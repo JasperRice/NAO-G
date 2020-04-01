f7b51845e2ad7ada0491fadbf94a8e55bf29165d
Sifan Jiang <jasper.sifan@gmail.com>
2020-04-01 20:30:40 +0200

    update data_processing

--- Code/human2robot/data_processing.py
+++ Code/human2robot/data_processing.py
@@ -1,7 +1,24 @@
+from sklearn.preprocessing import StandardScaler
+input_scaler = StandardScaler()
+output_scaler = StandardScaler()
+
+
+
 
 if __name__ == "__main__":
-    from sklearn.datasets import load_breast_cancer
-    breast_cancer = load_breast_cancer()
-    breast_cancer_data = breast_cancer.data
+    TEST = True
+    if TEST:
+        from sklearn.datasets import load_breast_cancer
+        breast_cancer = load_breast_cancer()
+        breast_cancer_data = breast_cancer.data
 
-    print breast_cancer_data.shape
+        input_scaler.fit(breast_cancer_data)
+        breast_cancer_data_normalized = input_scaler.transform(breast_cancer_data)
+
+        breast_cancer_data_denormalized = input_scaler.inverse_transform(breast_cancer_data_normalized)
+
+        print breast_cancer_data[0]
+        print breast_cancer_data_normalized[0]
+        print breast_cancer_data_denormalized[0]
+    else:
+        pass
