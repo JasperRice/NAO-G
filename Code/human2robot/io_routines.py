f0ecaa9a11379c4459f8e18615f39ab8ec6d2ce3
Sifan Jiang <jasper.sifan@gmail.com>
2020-04-02 18:29:06 +0200

    Use pandas to read csv

--- Code/human2robot/io_routines.py
+++ Code/human2robot/io_routines.py
@@ -1,5 +1,17 @@
+# HUMAN.csv is defined in degree
+# NAO.csv is defined in rad
+
+import pandas as pd
+
 def readCSV(filename):
-    f = open(filename)
+    df = pd.read_csv(filename)
+    return df
 
 def writeCSV(filename):
     pass
+
+if __name__ == "__main__":
+    df = readCSV("NAO.csv")
+    a = df.to_numpy()
+    print df.head(5)
+    print a
