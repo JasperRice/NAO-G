a662c40e2a208d7ce513b36b41c1bb8fbe8ad5e8
JasperRice <jasper.sifan@gmail.com>
2020-04-13 23:32:43 +0200

    Add sigmoid and tanh

--- Code/human2robot/network.py
+++ Code/human2robot/network.py
@@ -20,11 +20,13 @@ class Net(nn.Module):
         # Define each layer here:
         self.input2hidden = nn.Linear(n_input, n_hidden)
         self.hidden2output = nn.Linear(n_hidden, n_output)
-        self.drop = nn.Dropout(0.25)
+        self.dropout = nn.Dropout(0.25)
 
 
     def forward(self, x):
-        x = F.relu(self.drop(self.input2hidden(x)))
+        # x = F.relu(self.dropout(self.input2hidden(x)))
+        x = F.sigmoid(self.dropout(self.input2hidden(x)))
+        # x = F.tanh(self.dropout(self.input2hidden(x)))
         x = self.hidden2output(x)
         return x
 
