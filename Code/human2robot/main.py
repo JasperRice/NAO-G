3a216d8be36363f526023d3880889fd79c68503f
Sifan Jiang <jasper.sifan@gmail.com>
2020-04-14 01:03:21 +0200

    Remove hands in nao data

--- Code/human2robot/main.py
+++ Code/human2robot/main.py
@@ -2,10 +2,12 @@ from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-from human2robot.data_processing import decompose, normalize, split
-from human2robot.execute import execGesture
-from human2robot.io_routines import readCSV, saveNetwork
-from human2robot.network import Net
+import sys
+sys.path.append('human2robot/')
+from data_processing import decompose, normalize, split
+# from execute import execGesture
+from io_routines import readCSV, saveNetwork
+from network import Net
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -20,11 +22,14 @@ if __name__ == "__main__":
     # Whole data "talk"
     talk_01 = readCSV("human2robot/dataset/TALK_01.csv")
     talk_02 = readCSV("human2robot/dataset/TALK_02.csv")
+    talk_04 = readCSV("human2robot/dataset/TALK_04.csv")
+    talk_05 = readCSV("human2robot/dataset/TALK_05.csv")
     talk = np.vstack((talk_01, talk_02))
 
     # Normalize and decompose the dataset
     human = readCSV("human2robot/dataset/HUMAN.csv")
     nao = readCSV("human2robot/dataset/NAO.csv")
+    nao = nao[:,2:] # Remove hands
     n_human, _ = np.shape(human)
     n_nao, _ = np.shape(nao)
     if n_human is not n_nao:
@@ -35,7 +40,8 @@ if __name__ == "__main__":
     talk_n, _ = normalize(talk)
     _, talk_pca = decompose(talk_n)
     human_n, human_scaler = normalize(human)
-    human_n_d = talk_pca.transform(human_n)
+    # human_n_d, human_pca = decompose(human_n) # Use human to decompose
+    human_n_d = talk_pca.transform(human_n) # Use talk to decompose
     
     nao_n, nao_scaler = normalize(nao)
     nao_n_d, nao_pca = decompose(nao_n)
@@ -56,7 +62,8 @@ if __name__ == "__main__":
     nao_test_torch = torch.from_numpy(nao_test).float()
 
     # Define Neural Network and train
-    net = Net(n_input=talk_pca.n_components_, n_hidden=250, n_output=nao_pca.n_components_)
+    net = Net(n_input=human_pca.n_components_, n_hidden=250, n_output=nao_pca.n_components_)
+    # net = Net(n_input=talk_pca.n_components_, n_hidden=250, n_output=nao_pca.n_components_)
     optimizer = torch.optim.SGD(net.parameters(), lr=0.1)
     loss_func = nn.MSELoss()
 
@@ -71,7 +78,8 @@ if __name__ == "__main__":
         val_err = loss_func(net(human_val_torch), nao_val_torch)
         
         if val_err > old_val_err:
-            break
+            # break
+            pass
         old_val_err = val_err
 
         optimizer.zero_grad()
