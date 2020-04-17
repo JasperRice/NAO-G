e454368034ef5360e37e1266c4d962aec515b933
JasperRice <jasper.sifan@gmail.com>
2020-04-17 15:34:29 +0200

    Debugging

--- Code/human2robot/execute.py
+++ Code/human2robot/execute.py
@@ -4,22 +4,25 @@ import time
 
 start = time.time()
 
-def execGesture(IP, PORT, GESTURE, TIME=None):
+def execGesture(IP, PORT, JOINT, TIME=None):
     """Execute a static gesture on virtual or physical NAO robot
     
     :param IP: Adress IP of the NAO robot
     :type IP: str
     :param PORT: Port number of the NAO robot
     :type PORT: int
-    :param GESTURE: A list of lists of joint angles of NAO
-    :type GESTURE: list[list]
+    :param JOINT: A list of lists of joint angles of NAO
+    :type JOINT: list[list]
     :param TIME: A list of lists of time
     :type TIME: list[list]
     """
     motion = ALProxy("ALMotion", IP, PORT)
     if TIME == None:
-        TIME = [[5 * (i+1)] * len(GESTURE[0]) for i in range(len(GESTURE))]
-    motion.angleInterpolation("Joints", GESTURE, TIME, True)
+        TIME = [[5 * (i+1)] * len(JOINT[0]) for i in range(len(JOINT))]
+
+    print(len(JOINT), len(JOINT[0]))
+    print(len(TIME), len(TIME[0]))
+    motion.angleInterpolation("Joints", JOINT, TIME, True)
 
 
 if __name__ == "__main__":
