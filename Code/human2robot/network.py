7409b2df5500da2aa8f7a65073a2db6171286530
JasperRice <jasper.sifan@gmail.com>
2020-04-29 15:23:00 +0200

    Add a CutAngle at the end of the network

--- Code/human2robot/network.py
+++ Code/human2robot/network.py
@@ -2,6 +2,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class CutAngle(nn.Module):
+    def __init__(self, upper, lower):
+        """[summary]
+
+        :param nn: [description]
+        :type nn: [type]
+        :param upper: [description]
+        :type upper: [type]
+        :param lower: [description]
+        :type lower: [type]
+        """
+        super(CutAngle, self).__init__()
+        self.upper = upper
+        self.lower = lower
+
+    def forward(self, x):
+        upper_index = x > self.upper
+        lower_index = x < self.lower
+        x[upper_index] = self.upper[upper_index]
+        x[lower_index] = self.lower[lower_index]
+        return x
+
 
 class Net(nn.Module):
     def __init__(self, n_input, n_hidden, n_output, AF='relu', dropout_rate=0):
@@ -17,7 +39,7 @@ class Net(nn.Module):
         :type AF: str, optional
         :param dropout_rate: The dropout rate of the hidden layer, defaults to 0
         :type dropout_rate: int, optional
-        """   
+        """
         super(Net, self).__init__()
 
         self.activations = nn.ModuleDict({
@@ -57,9 +79,8 @@ class Net(nn.Module):
 
 
 class MultiLayerNet(nn.Module):
-    def __init__(self, n_input, n_hiddens, n_output, AF='relu', dropout_rate=0):
+    def __init__(self, n_input, n_hiddens, n_output, joint_upper, joint_lower, AF='relu', dropout_rate=0):
         """The feed forward neural network with multiple hidden layers
-        TODO: Add threshold layer at the end of the net
         
         :param n_input: The dimension of the input layer
         :type n_input: int
@@ -67,11 +88,15 @@ class MultiLayerNet(nn.Module):
         :type n_hidden: list[int]
         :param n_output: The dimension of the output layer
         :type n_output: int
+        :param joint_upper: The upper bound of allowed joints of NAO (after Normalization)
+        :type joint_upper: np.ndarray
+        :param joint_lower: The lower bound if allowed joints of NAO (after Normalization)
+        :type joint_lower: np.ndarray
         :param AF: The activation function to be used, defaults to 'relu'
         :type AF: str, optional
         :param dropout_rate: The dropout rate of the hidden layer, defaults to 0
         :type dropout_rate: int, optional
-        """   
+        """
         super(MultiLayerNet, self).__init__()
 
         self.activations = nn.ModuleDict({
@@ -96,32 +121,14 @@ class MultiLayerNet(nn.Module):
         self.LayerList.extend(nn.Linear(n_hiddens[i], n_hiddens[i+1]) for i in range(len(n_hiddens)-1))
         # self.LayerList.extend(nn.Linear(n_hiddens[-1], n_output))
         self.hidden2output = nn.Linear(n_hiddens[-1], n_output)
+        self.cutAngle = CutAngle(joint_upper, joint_lower)
         self.AF = self.activations[AF]
         self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, x):
         for layer in self.LayerList:
             x = self.AF(self.dropout(layer(x)))
-        x = self.hidden2output(x)
-        return x
-
-
-class CutAngleLayer(nn.Module):
-    def __init__(self, upper, lower):
-        """[summary]
-
-        :param nn: [description]
-        :type nn: [type]
-        :param upper: [description]
-        :type upper: [type]
-        :param lower: [description]
-        :type lower: [type]
-        """        
-        super(CutAngleLayer, self).__init__()
-        self.upper = upper
-        self.lower = lower
-
-    def forward(self, x):
+        x = self.cutAngle(self.hidden2output(x))
         return x
 
 
@@ -133,5 +140,5 @@ def numpy2tensor(x):
     :type x: numpy.ndarray
     :return: The torch data after transformation
     :rtype: torch.Tensor
-    """    
+    """
     return torch.from_numpy(x).float()
