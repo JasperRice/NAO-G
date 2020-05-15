79a9da30957014e31d7bfe8bb4595ae7a44ae7a2
JasperRice <jasper.sifan@gmail.com>
2020-05-15 16:40:55 +0200

    Update NAO.py to enable transform angles from NAO to human

--- Code/human2robot/NAO.py
+++ Code/human2robot/NAO.py
@@ -1,10 +1,13 @@
+from copy import deepcopy
+from math import degrees
 from naoqi import ALProxy
 from torch import nn
 import matplotlib.pyplot as plt
-import math
 import numpy as np
+import pandas as pd
 import torch
 
+from Human import HumanInterface
 from io_routines import readCSV
 from network import Net
 from setting import *
@@ -24,14 +27,32 @@ class NAOInterface:
             'maxTorque':    limits[:, 3]
         }
         self.jointAngles = []
+        self.jointAnglesBackup = []
         try: self.net = Net(**net_kwargs)
         except: pass
 
+    def __str__(self):
+        return ' '.join(self.joint_names)
+
     def readFromCSV(self, filenameList):
-        if filenameList is str:
+        self.jointAngles = []
+        if type(filenameList) is str:
             filenameList = [filenameList]
         for filename in filenameList:
-            pass
+            df = pd.read_csv(filename)
+            self.jointAngles += df.values.tolist()
+        self.jointAnglesBackup = deepcopy(self.jointAngles)
+
+    def writeToCSV(self, filename, mode='a'):
+        file = open(filename, mode=mode)
+        if mode == 'w':
+            file.write(', '.join(self.joint_names)+'\n') 
+            for angle in self.jointAngles:
+                file.write(', '.join(list(map(str, angle)))+'\n')
+        elif mode == 'a':
+            for angle in self.jointAngles:
+                file.write(', '.join(list(map(str, angle)))+'\n')
+        file.close()
 
     def getJointAngleDegrees(self):
         for joint in self.jointAngles:
@@ -47,8 +68,49 @@ class NAOInterface:
         print('=====> Upper bound:')
         print(self.limits['maxAngle'])
 
-    def addNewJointAngles(self):
-        self.jointAngles.append(self.motion.getAngles(self.jointSetName, True))
+    def getJointIndex(self, jointName):
+        return self.joint_names.index(jointName)
+
+    def getCurrentJointAngle(self):
+        return self.motion.getAngles(self.jointSetName, True)
+
+    def addCurrentJointAngleToList(self):
+        self.jointAngles.append(self.getCurrentJointAngle())
+
+    def transformJointAnglesListToHuman(self, human):
+        for jointAngle in self.jointAngles:
+            humanJointAngle = self.transformJointAnglesToHuman(human, jointAngle)
+            human.jointAngles.append(humanJointAngle)
+
+    def transformJointAnglesToHuman(self, human, jointAngle):
+        humanJointAngle = [0] * len(human)
+        humanJointAngle[human.getStartAngleIndex('RightShoulder')] = 90
+        humanJointAngle[human.getStartAngleIndex('LeftShoulder')] = -90
+        naoJointList = [
+            'RShoulderRoll', 'LShoulderRoll', 'RShoulderPitch', 'LShoulderPitch',
+            'RElbowRoll', 'LElbowRoll', 'RElbowYaw', 'LElbowYaw',
+            'RWristYaw', 'LWristYaw'
+        ]
+        humanJointList = [
+            ['RightArm', 0], ['LeftArm', 0], ['RightArm', 1], ['LeftArm', 1],
+            ['RightForeArm', 1], ['LeftForeArm', 1], ['RightForeArm', 2], ['LeftForeArm', 2],
+            ['RightHand', 2], ['LeftHand', 2]
+        ]
+        for naoJoint, humanJoint in zip(naoJointList, humanJointList):
+            naoIndex = self.getJointIndex(naoJoint)
+            humanIndex = human.getStartAngleIndex(humanJoint[0]) + humanJoint[1]
+            if naoJoint in ['RShoulderRoll', 'LElbowYaw']:
+                humanJointAngle[humanIndex] = degrees(jointAngle[naoIndex]) + 90
+            elif naoJoint in ['LShoulderRoll', 'RElbowYaw']:
+                humanJointAngle[humanIndex] = degrees(jointAngle[naoIndex]) - 90
+            elif naoJoint in ['RShoulderPitch', 'LShoulderPitch']:
+                humanJointAngle[humanIndex] = - degrees(jointAngle[naoIndex]) + 90
+            elif naoJoint in ['RElbowRoll', 'RWristYaw', 'LWristYaw']:
+                humanJointAngle[humanIndex] = degrees(jointAngle[naoIndex])
+            elif naoJoint in ['LElbowRoll']:
+                humanJointAngle[humanIndex] = - degrees(jointAngle[naoIndex])
+            # print(naoJoint, naoIndex, degrees(jointAngle[naoIndex]), humanJoint, humanIndex, humanJointAngle[humanIndex])
+        return humanJointAngle
 
     def loadNetwork(self, PATH):
         # self.net = xxx.load_state_dict(torch.load(PATH))
@@ -77,6 +139,17 @@ class NAOInterface:
     def executePoses(self):
         pass
 
+    def executePosesOneByOne(self, index=None):
+        T = [0.5] * len(self.joint_names)
+        if index:
+            print("=====> Pose index: %d." % (index))
+            self.motion.angleInterpolation(self.jointSetName, self.joint_names[index], T, True)
+        else:
+            for index, angle in enumerate(self.jointAngles):
+                print("=====> Pose index: %d." % (index))
+                self.motion.angleInterpolation(self.jointSetName, angle, T, True)
+                raw_input("Press ENTER to continue ...")
+
     def generatePoses(self):
         pass
 
@@ -91,9 +164,26 @@ class NAOInterface:
 
 
 if __name__ == "__main__":
+    human = HumanInterface.createFromBVH('/home/nao/Documents/NAO-G/Code/human2robot/human_skeletion.bvh')
     nao = NAOInterface(IP=NAO_IP, PORT=NAO_PORT)
+    print(nao)
     # nao.stand()
 
-    nao.addNewJointAngles()
-    print(nao.jointAngles)
-    nao.getJointAngleDegrees()
+    # CONTINUE = True
+    # while CONTINUE:
+    #     nao.addCurrentJointAngleToList()
+    #     if_continue = raw_input("If continue to record current joint angle (y/n):")
+    #     if if_continue in ['y', 'Y', 'yes']:
+    #         CONTINUE = True
+    #     elif if_continue in ['n', 'N', 'no']:
+    #         CONTINUE = False
+    #     else:
+    #         if_continue = raw_input("Input 'y' or 'n': ", end='')
+    # nao.writeToCSV('/home/nao/Documents/NAO-G/Code/human2robot/dataset/NAO_extra.csv', mode='a')
+    # nao.transformJointAnglesListToHuman(human)
+    # human.writeToBVH('/home/nao/Documents/NAO-G/Code/human2robot/Human_extra.bvh')
+
+    # nao.readFromCSV('/home/nao/Documents/NAO-G/Code/human2robot/dataset/NAO_extra.csv')
+    # nao.transformJointAnglesListToHuman(human)
+    # human.writeToBVH('/home/nao/Documents/NAO-G/Code/human2robot/Human_extra.bvh')
+    # nao.executePosesOneByOne()
