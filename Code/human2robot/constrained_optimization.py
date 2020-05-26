b6830a5f351a12a02774af4d7844e7278a23c02c
Sifan Jiang <jasper.sifan@gmail.com>
2020-05-27 00:00:38 +0200

    Rename

--- Code/human2robot/constrained_optimization.py
+++ Code/human2robot/constrained_optimization.py
@@ -2,49 +2,56 @@ import numpy as np
 from scipy.optimize import minimize
 
 
-class cons(object):
+class constrained_optimization:
+    
+    def __init__(self, nao_interface, nao_motion):
+        self.limits = nao_interface.limits
+        self.motion = nao_motion
+
+def linearQuaratic(x_opt, *x):
     pass
 
 
-def calcVolume(x):
-    length = x[0]
-    width = x[1]
-    height = x[2]
-    volume = length * width * height
-    return volume
+if __name__ == "__main__":
+    def calcVolume(x):
+        length = x[0]
+        width = x[1]
+        height = x[2]
+        volume = length * width * height
+        return volume
 
-def calcSurface(x):
-    length = x[0]
-    width = x[1]
-    height = x[2]
-    surface = 2 * (length*width + width*height + height*length)
-    return surface
+    def calcSurface(x):
+        length = x[0]
+        width = x[1]
+        height = x[2]
+        surface = 2 * (length*width + width*height + height*length)
+        return surface
 
-def objective(x):
-    return -calcVolume(x)
+    def objective(x):
+        return -calcVolume(x)
 
-def constraint(x):
-    return 10 - calcSurface(x)
+    def constraint(x):
+        return 10 - calcSurface(x)
 
-def length_constraint(x):
-    return 1 - x[0]
+    def length_constraint(x):
+        return 1 - x[0]
 
-cons = ({
-    'type': 'ineq',
-    'fun': constraint
-})
+    cons = ({
+        'type': 'ineq',
+        'fun': constraint
+    })
 
-len_cons = ({
-    'type': 'ineq',
-    'fun': lambda x: 1 - x[0]
-})
+    len_cons = ({
+        'type': 'ineq',
+        'fun': lambda x: 1 - x[0]
+    })
 
-# Initial guess
-lengthGuess = 1
-widthGuess = 1
-heightGuess = 1
+    # Initial guess
+    lengthGuess = 1
+    widthGuess = 1
+    heightGuess = 1
 
-x0 = np.array([lengthGuess, widthGuess, heightGuess])
+    x0 = np.array([lengthGuess, widthGuess, heightGuess])
 
-sol = minimize(objective, x0, method='SLSQP', constraints=[cons, len_cons], options={'disp': True})
-print(sol.x)
+    sol = minimize(objective, x0, method='SLSQP', constraints=[cons, len_cons], options={'disp': True})
+    print(sol.x)
