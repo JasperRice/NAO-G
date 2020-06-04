c4468da75801746684c29d1c73acc95b9006bc44
JasperRice <jasper.sifan@gmail.com>
2020-06-04 18:33:05 +0200

    Record finger index in human motion

--- Code/human2robot/main_new.py
+++ Code/human2robot/main_new.py
@@ -33,13 +33,34 @@ if __name__ == "__main__":
         try: nao_interface = NAOInterface(IP=NAO_IP, PORT=NAO_PORT)
         except: pass
 
+    fingerIndex = []
+    fingerJointList = [
+            'RightHandThumb1',   'RightHandThumb2',     'RightHandThumb3',
+            'RightHandIndex1',   'RightHandIndex2',     'RightHandIndex3',
+            'RightHandMiddle1',  'RightHandMiddle2',    'RightHandMiddle3',
+            'RightHandRing1',    'RightHandRing2',      'RightHandRing3',
+            'RightHandPinky1',   'RightHandPinky2',     'RightHandPinky3',
+            'LeftHandThumb1',    'LeftHandThumb2',      'LeftHandThumb3',
+            'LeftHandIndex1',    'LeftHandIndex2',      'LeftHandIndex3',
+            'LeftHandMiddle1',   'LeftHandMiddle2',     'LeftHandMiddle3',
+            'LeftHandRing1',     'LeftHandRing2',       'LeftHandRing3',
+            'LeftHandPinky1',    'LeftHandPinky2',      'LeftHandPinky3',
+        ]
+    for fingerJoint in fingerJointList:
+        index = human_interface.getStartAngleIndex(fingerJoint)
+        fingerIndex.extend([i for i in range(index, index+3)])
+
     # Read dataset
     talk_list = map(readCSV, talkfile)
     talk = np.vstack(talk_list)
     human = readCSV('dataset/Human.csv')
     human_new = readCSV('dataset/Human_new.csv')
-    human_new_agu = human_new + np.random.normal(loc=0.0, scale=0.6, size=np.shape(human_new))
     human_right_hand = readCSV('dataset/Human_right_hand.csv')
+
+    for motion in [human, human_new, human_right_hand]
+
+
+    human_new_agu = human_new + np.random.normal(loc=0.0, scale=0.6, size=np.shape(human_new))
     human_right_hand_agu = human_right_hand + np.random.normal(loc=0.0, scale=0.6, size=np.shape(human_right_hand))
     human = np.vstack([
         # human,
