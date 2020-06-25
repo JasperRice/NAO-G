5fc28693a04be2ee5f1a2471dd8ecfd36b420765
JasperRice <jasper.sifan@gmail.com>
2020-06-25 18:05:22 +0200

    New script for testing the effect of data size on the test result

---
+++ Code/human2robot/size_test.py
@@ -0,0 +1,86 @@
+from scipy.optimize import minimize
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+
+import sys
+sys.path.append('human2robot/')
+
+from constrained_optimization import constraints, objective, piecewise_constraints, piecewise_objective
+try: from execute import execGesture
+except: pass
+from data_processing import decompose, normalize, split, smooth
+from Human import HumanInterface
+from io_routines import readCSV, saveNetwork
+try: from NAO import NAOInterface
+except: pass
+from network import Net, numpy2tensor
+from setting import *
+
+def choices(N, n):
+    token = list(range(N))
+    res = []
+    for _ in range(n):
+        ele = random.choice(token)
+        token.remove(ele)
+        res.append(ele)
+    return res
+
+
+torch.manual_seed(2020)
+human_interface = HumanInterface.createFromBVH('dataset/BVH/human_skeletion.bvh')
+try: nao_interface = NAOInterface(IP=P_NAO_IP, PORT=P_NAO_PORT)
+except:
+    try: nao_interface = NAOInterface(IP=NAO_IP, PORT=NAO_PORT)
+    except: pass
+
+fingerIndex = []
+fingerJointList = ['RightHandThumb1',   'RightHandThumb2',     'RightHandThumb3',
+                   'RightHandIndex1',   'RightHandIndex2',     'RightHandIndex3',
+                   'RightHandMiddle1',  'RightHandMiddle2',    'RightHandMiddle3',
+                   'RightHandRing1',    'RightHandRing2',      'RightHandRing3',
+                   'RightHandPinky1',   'RightHandPinky2',     'RightHandPinky3',
+                   'LeftHandThumb1',    'LeftHandThumb2',      'LeftHandThumb3',
+                   'LeftHandIndex1',    'LeftHandIndex2',      'LeftHandIndex3',
+                   'LeftHandMiddle1',   'LeftHandMiddle2',     'LeftHandMiddle3',
+                   'LeftHandRing1',     'LeftHandRing2',       'LeftHandRing3',
+                   'LeftHandPinky1',    'LeftHandPinky2',      'LeftHandPinky3',
+                ]
+for fingerJoint in fingerJointList:
+    index = human_interface.getStartAngleIndex(fingerJoint)
+    fingerIndex.extend([i for i in range(index, index+3)])
+
+# Read dataset
+talk_list = map(readCSV, talkfile)
+talk = np.vstack(talk_list); talk = np.delete(talk, fingerIndex, axis=1)
+talk, _ = normalize(talk); _, human_pca = decompose(talk)
+
+human_data = readCSV('dataset/Human_overlap.csv'); human_data = np.delete(human_data, fingerIndex, axis=1)
+human_test = readCSV('dataset/Human_test.csv'); human_test = np.delete(human_test, fingerIndex, axis=1)
+nao_data = readCSV('dataset/NAO_overlap.csv'); nao_test = readCSV('dataset/NAO_test.csv')
+n = np.size(human_data, 0)
+if n != np.size(nao_data, 0):
+    sys.exit("Numbers of input and target are different.")
+
+results = {}
+for i in [25, 50, 75, 100, 125]:
+    random.seed(1000)
+    mask = choices(n, i)
+    
+    human = human_data[mask]
+    human, human_scaler = normalize(human); human = human_pca.transform(human)
+
+    nao = nao_data[mask]
+    nao, nao_scaler = normalize(nao)
+
+    human_train, human_val, nao_train, nao_val = train_test_split(human, nao, test_size=0.2, random_state=500)
+
+    for j in range(20):
+        pass
+    
