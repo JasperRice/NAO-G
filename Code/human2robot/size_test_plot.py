0d47d381f09edb8b147e4450a696c9b39b41a731
JasperRice <jasper.sifan@gmail.com>
2020-06-29 13:30:30 +0200

    Use all the 80 size data to plot a more accurate plot

--- Code/human2robot/size_test_plot.py
+++ Code/human2robot/size_test_plot.py
@@ -1,17 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-file = open("size_test.csv", 'r')
-lines = file.readlines()
+Xs = [25, 50, 75, 100, 125]
+
+file_01 = open("dataset/size_test_01.csv", 'r')
+file_02 = open("dataset/size_test_02.csv", 'r')
+file_03 = open("dataset/size_test_03.csv", 'r')
+file_04 = open("dataset/size_test_04.csv", 'r')
+# files = [file_01, file_02, file_03, file_04]
+files = [file_04]
+
+temp = [[] for _ in range(len(Xs))]
 y = []
 e = []
-for line in lines:
-    wordList = line.split(', ')
-    testResults = np.array(map(float, wordList))
-    y.append(np.mean(testResults))
-    e.append(np.std(testResults))
 
-x = np.array([25, 50, 75, 100, 125])
+for file in files:
+    lines = file.readlines()
+    for i, line in enumerate(lines):
+        wordList = line.split(', ')
+        temp[i] += list(map(float, wordList))
+    file.close()
+
+for t in temp:
+    t_arr = np.array(t)
+    y.append(np.mean(t_arr))
+    e.append(np.std(t_arr))
+
+x = np.array(Xs)
 y = np.array(y)
 e = np.array(e)
 
