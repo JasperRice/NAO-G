c8554d7040faf9ca94726189522fc5b0e6b16519
JasperRice <jasper.sifan@gmail.com>
2020-07-16 19:11:06 +0200

    Update main.py, without using so many if statements

--- Code/human2robot/main.py
+++ Code/human2robot/main.py
@@ -1,199 +1,90 @@
+from scipy.optimize import minimize
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-import sys
-sys.path.append('human2robot/')
-try:
-    from execute import execGesture
-except:
-    pass
-from data_processing import decompose, normalize, split, smooth
-from io_routines import readCSV, saveNetwork
-# from NAO import NAOInterface
-from network import Net, numpy2tensor
-from setting import *
-
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 import torch
 import torch.nn as nn
+
+import sys
+sys.path.append('human2robot/')
+
+from constrained_optimization import constraints, objective, piecewise_constraints, piecewise_objective
+try: from execute import execGesture
+except: pass
+from data_processing import decompose, normalize, split, smooth
+from Human import HumanInterface
+from io_routines import readCSV, saveNetwork
+try: from NAO import NAOInterface
+except: pass
+from network import Net, numpy2tensor
+from setting import *
 
 
 if __name__ == "__main__":
     # Reproducibility by setting a seed
     torch.manual_seed(0)
 
+    # Create interface for Human and NAO
+    # Human interface is used for recording the finger indices
+    human_interface = HumanInterface.createFromBVH('dataset/BVH/human_skeletion.bvh')
+    try: nao_interface = NAOInterface(IP=P_NAO_IP, PORT=P_NAO_PORT)
+    except:
+        try: nao_interface = NAOInterface(IP=NAO_IP, PORT=NAO_PORT)
+        except: pass
+    fingerIndex = []
+    fingerJointList = [
+            'RightHandThumb1',   'RightHandThumb2',     'RightHandThumb3',
+            'RightHandIndex1',   'RightHandIndex2',     'RightHandIndex3',
+            'RightHandMiddle1',  'RightHandMiddle2',    'RightHandMiddle3',
+            'RightHandRing1',    'RightHandRing2',      'RightHandRing3',
+            'RightHandPinky1',   'RightHandPinky2',     'RightHandPinky3',
+            'LeftHandThumb1',    'LeftHandThumb2',      'LeftHandThumb3',
+            'LeftHandIndex1',    'LeftHandIndex2',      'LeftHandIndex3',
+            'LeftHandMiddle1',   'LeftHandMiddle2',     'LeftHandMiddle3',
+            'LeftHandRing1',     'LeftHandRing2',       'LeftHandRing3',
+            'LeftHandPinky1',    'LeftHandPinky2',      'LeftHandPinky3',
+        ]
+    for fingerJoint in fingerJointList:
+        index = human_interface.getStartAngleIndex(fingerJoint)
+        fingerIndex.extend([i for i in range(index, index+3)])
 
-    # Read dataset
-    if USE_TALK:
-        talk_list = map(readCSV, filename[:-2])
-        talk = np.vstack(talk_list)
-    human = readCSV(filename[-2])
-    nao = readCSV(filename[-1]) if USE_HAND else readCSV(filename[-1])[:,2:]
-    # Test gestures
-    # execGesture(NAO_IP, NAO_PORT, nao[:,2:].tolist()) \
-            # if USE_HAND else execGesture(NAO_IP, NAO_PORT, nao.tolist())
-    
-    n = np.size(human, 0)
-    if n != np.size(nao, 0):
+    # Read traing and test dataset
+    human = readCSV('dataset/Human_overlap.csv'); human = np.delete(human, fingerIndex, axis=1)
+    human_test = readCSV('dataset/Human_test.csv'); human_test = np.delete(human_test, fingerIndex, axis=1)
+    nao = readCSV('dataset/NAO_overlap.csv')
+    nao_test = readCSV('dataset/NAO_test.csv')
+    if np.size(human, 0) != np.size(nao, 0):
         sys.exit("Numbers of input and target are different.")
 
-
     # Normalize and decompose the dataset
-    if NORMALIZE:
-        human, human_scaler = normalize(human)
-        nao, nao_scaler = normalize(nao)
-        if USE_TALK:
-            talk, _ = normalize(talk)
-
-    if DECOMPOSE:
-        if USE_TALK:
-            _, human_pca = decompose(talk)
-            human = human_pca.transform(human)
-        else:
-            human, human_pca = decompose(human)
-
-        nao, nao_pca = decompose(nao)
-
+    human, human_scaler = normalize(human)
+    human_test = human_scaler.transform(human_test)
+    nao, nao_scaler = normalize(nao)
+    nao_test = nao_scaler.transform(nao_test)
 
     # Split the dataset into train, test, and validation
-    dataset = split(human, nao)
+    human_train, human_val, nao_train, nao_val = train_test_split(human, nao, test_size=0.2, random_state=1000)
+    human_train_torch   = torch.from_numpy(human_train).float()
+    human_val_torch     = torch.from_numpy(human_val).float()
+    human_test_torch    = torch.from_numpy(human_test).float()
+    nao_train_torch     = torch.from_numpy(nao_train).float()
+    nao_val_torch       = torch.from_numpy(nao_val).float()
+    nao_test_torch      = torch.from_numpy(nao_test).float()
 
+    # Save the shuffled train, validation data
+    human_train_save = human_scaler.inverse_transform(human_train); np.savetxt("shuffled_human_train.txt", human_train_save)
+    human_val_save = human_scaler.inverse_transform(human_val);     np.savetxt("shuffled_human_train.txt", human_val_save)
+    human_test_save = human_scaler.inverse_transform(human_test);   np.savetxt("shuffled_human_train.txt", human_test_save)
+    nao_train_save = human_scaler.inverse_transform(nao_train);     np.savetxt("shuffled_human_train.txt", nao_train_save)
+    nao_val_save = human_scaler.inverse_transform(nao_val);         np.savetxt("shuffled_human_train.txt", nao_val_save)
+    nao_test_save = human_scaler.inverse_transform(nao_test);       np.savetxt("shuffled_human_train.txt", nao_test_save)
 
-    # Save the shuffled train, test, and validation data and visualize the ground truth of the NAO gestures
-    if SAVE_DATA:
-        human_to_save = dataset[0:3]
-        nao_to_save = dataset[3:]
-        try:
-            human_to_save = map(human_pca.inverse_transform, human_to_save)
-            nao_to_save = map(nao_pca.inverse_transform, nao_to_save)
-        except NameError:
-            pass
-        try:
-            human_to_save = map(human_scaler.inverse_transform, human_to_save)
-            nao_to_save = map(nao_scaler.inverse_transform, nao_to_save)
-        except NameError:
-            pass
-        np.savetxt("human_train.txt", human_to_save[0])
-        np.savetxt("human_val.txt", human_to_save[1])
-        np.savetxt("human_test.txt", human_to_save[2])
-        np.savetxt("nao_train.txt", nao_to_save[0])
-        np.savetxt("nao_val.txt", nao_to_save[1])
-        np.savetxt("nao_test.txt", nao_to_save[2])
-        execGesture(NAO_IP, NAO_PORT, nao_to_save[ON_SET][:,2:].tolist()) \
-            if USE_HAND else execGesture(NAO_IP, NAO_PORT, nao_to_save[ON_SET].tolist())
-        exit()
-
-
-    # Transfer the numpy to tensor in pytorch
-    dataset_torch = map(numpy2tensor, dataset)
-    human_train_torch = dataset_torch[0]
-    human_val_torch = dataset_torch[1]
-    human_test_torch = dataset_torch[2]
-    nao_train_torch = dataset_torch[3]
-    nao_val_torch = dataset_torch[4]
-    nao_test_torch = dataset_torch[5]
-
-    # nao_interface = Interface(NAO_IP, NAO_PORT)
-    # Define Neural Network and train
-    net = Net(n_input=np.size(human, 1),
-                n_hidden=N_HIDDEN,
-                n_output=np.size(nao, 1),
-                AF=AF, dropout_rate=DO_RATE
-                )
-    # net = Net(n_input=np.size(human, 1), n_hidden=N_HIDDEN, n_output=np.size(nao, 1), AF=AF, dropout_rate=DO_RATE)
-    optimizer = torch.optim.SGD(net.parameters(), lr=L_RATE)
-    loss_func = nn.MSELoss()
-
-
-    # Main loop for training
-    min_error = np.inf
-    for epoch in range(MAX_EPOCH):
-        print("=====> Epoch: "+str(epoch+1))
-
-        # Train
-        net.train()
-        prediction = net(human_train_torch)
-        loss = loss_func(prediction, nao_train_torch)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # Validation error
-        net.eval()
-        val_err = loss_func(net(human_val_torch), nao_val_torch)
-        
-        if STOP_EARLY:
-            if val_err - min_error > STOP_RATE * min_error:
-                break
-            elif val_err < min_error:
-                min_error = val_err
-
-        # Plot the error
-        plt.scatter(epoch, loss.data.numpy(), s=1, c='r')
-        plt.scatter(epoch, val_err.data.numpy(), s=1, c='g')
-
-    plt.show()
-    plt.savefig(AF+'_LR='+str(L_RATE)+'_Hidden='+str(N_HIDDEN)+'_Normalize='+str(NORMALIZE)+'_Decompose='+str(DECOMPOSE)+'_Dropout='+str(DO_RATE)+'.png')
-
-
-    # Visualize result on NAO
-    if VISUALIZE:
-        net.eval()
-        prediction = net(dataset_torch[ON_SET])
-        nao_out = prediction.detach().numpy()
-        try:
-            nao_out = nao_pca.inverse_transform(nao_out)
-        except NameError:
-            pass
-        try:
-            nao_out = nao_scaler.inverse_transform(nao_out)
-        except NameError:
-            pass
-        execGesture(NAO_IP, NAO_PORT, nao_out[:,2:].tolist()) \
-            if USE_HAND else execGesture(NAO_IP, NAO_PORT, nao_out.tolist())
-
-
-    # Play the talk
-    if PLAY_TALK:
-        net.eval()
-        talk_play = readCSV(filename[PLAY_SET])
-        try:
-            talk_play = human_scaler.transform(talk_play)
-        except:
-            pass
-
-        try:
-            talk_play = human_pca.transform(talk_play)
-        except:
-            pass
-        
-        talk_play = numpy2tensor(talk_play)
-        prediction = net(talk_play)
-        nao_out = prediction.detach().numpy()
-        try:
-            nao_out = nao_pca.inverse_transform(nao_out)
-        except NameError:
-            pass
-        try:
-            nao_out = nao_scaler.inverse_transform(nao_out)
-        except NameError:
-            pass
-
-    smooth_kwargs = {
-        'window_length':    13,
-        'polyorder':        3,
-        'deriv':            0,
-        'delta':            1.0,
-        'axis':             -1,
-        'mode':             'interp',
-        'cval':             0.0
-    }
-    nao_out = smooth(nao_out, smoothing_method='savgol', **smooth_kwargs)
-    # execGesture(NAO_IP, NAO_PORT, nao_out[:,2:].tolist()) \
-    #     if USE_HAND else execGesture(NAO_IP, NAO_PORT, nao_out.tolist())
-    to_plot = nao_out.T.tolist()
-    plt.plot(to_plot[-1][:100], '-')
-    plt.show()
+    # Define Neural Network model and train
+    net = Net(n_input=np.size(human, 1), n_hidden=N_HIDDEN, n_output=np.size(nao, 1),
+              AF=AF, dropout_rate=DO_RATE, learning_rate=L_RATE, reg=REG, ues_lr_scheduler=False)
+    net.__train__(human_train_torch, human_val_torch, nao_train_torch, nao_val_torch)
+    net.__plot__()
